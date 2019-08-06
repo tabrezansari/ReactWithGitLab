9bfe7aca67b34535ac020da38ab663dd960466c2
Tabrez Ansari <tabrezsvce@gmail.com>
2019-08-06 11:38:15 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing ReactApp with GitLab CI/CD tst.
+          Testing ReactApp with GitLab CI/CD .
         </p>
         
          
